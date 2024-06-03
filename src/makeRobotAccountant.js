4c791705136fbcc283b39a1fc1cbec09bd095c6e
Єва <eva@Evas-MacBook-Air.local>
2024-06-03 18:46:50 +0100

    Done

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,23 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  function getSum(number) {
+    return function(value) {
+      count++;
+
+      const result = value + number;
+
+      if (count % 2 === 0 && count > 3) {
+        return 'Bzzz... Error!';
+      } else {
+        return result;
+      }
+    };
+  }
+
+  return getSum;
 }
 
 module.exports = makeRobotAccountant;
